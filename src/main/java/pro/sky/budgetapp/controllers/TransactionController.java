1f1151a7e3ec0de0ecbe581dadd5247067db58b9
Spolpinka <a421243266@gmail.com>
2023-03-09 15:30:10 +0300

    с подсчетом зарплаты вместе с отпускными + логика расчета ежедневного лимита и трат + rest API c PathVariable, RequestBody

--- src/main/java/pro/sky/budgetapp/controllers/TransactionController.java
+++ src/main/java/pro/sky/budgetapp/controllers/TransactionController.java
@@ -1,12 +1,25 @@
 package pro.sky.budgetapp.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pro.sky.budgetapp.model.Category;
 import pro.sky.budgetapp.model.Transaction;
 import pro.sky.budgetapp.services.BudgetService;
 
+import java.time.Month;
+
 @RestController
 @RequestMapping("/transaction")
+@Tag(name = "Транзакции", description = "Добавление, редактирование, получение, удаление")
 public class TransactionController {
 
     private final BudgetService budgetService;
@@ -38,6 +51,38 @@ public class TransactionController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(transactionById);
+    }
+
+    @GetMapping()
+    @Operation(
+            summary = "Поиск транзакций по месяцу и (или) категории",
+            description = "можно поискать по месяцу или категории или без фильтров совсем"
+    )
+    @Parameters(
+            value = {
+                    @Parameter(
+                            name = "month",
+                            description = "месяц из соответствующего Enum",
+                            example = "December"
+                    )
+            }
+    )
+    @ApiResponses (value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Транзакции найдены",
+                    content = {
+                            @Content (
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Transaction.class))
+                            )
+                    }
+            )
+    }
+    )
+    public ResponseEntity<Transaction> getAllTransactions(@RequestParam (required = false) Month month,
+                                                          @RequestBody (required = false)Category category) {
+        return null;
     }
 
     @DeleteMapping("/{id}")
